d4e2443f504dff2e881592e0086c9e89b94b9e43
xuwangdong <xuwangdong@boohee.com>
2022-10-30 16:12:36 +0800

    feat: 更新目录

--- constant.js
+++ constant.js
@@ -1,25 +1,39 @@
 const navbarPluginsInfo = [
   {
-    id: 'vue',
-    name: 'Vue 笔记',
+    id: "vue",
+    name: "Vue 笔记",
     // docId: '',
     // position: 'right',
   },
   {
-    id: 'styles',
-    name: '样式相关'
+    id: "styles",
+    name: "样式相关",
   },
   {
-    id: 'visual',
-    name: '可视化方案'
+    id: "visual",
+    name: "可视化方案",
   },
   {
-    id: 'deployed',
-    docId: 'deployed',
-    name: '构建部署'
-  }
-]
+    id: "engine_config",
+    name: "工程化配置",
+  },
+  {
+    id: "deployed",
+    docId: "deployed",
+    name: "构建部署",
+  },
+  {
+    id: "data_structure",
+    docId: "data_structure",
+    name: "数据结构与算法",
+  },
+  {
+    id: "interview",
+    docId: "interview",
+    name: "面试题",
+  },
+];
 
 module.exports = {
-  navbarPluginsInfo
-}
+  navbarPluginsInfo,
+};
---
+++ docs/engine_config/index.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro deployed 999
+
+Let's discover **Docusaurus in less than 5 minutes**.
